3a5bad797c1fb16fdf3b1239e87d92be9fe0cf6a
PokestarFan <azoysarkar@gmail.com>
2017-09-09 09:18:39 -0400

    Add logging

--- duplicate.py
+++ duplicate.py
@@ -1,9 +1,13 @@
 import datetime
+import logging
 import praw
 from login import reddit
 
+
+
 def action():
     for sub_id in reddit.subreddit('all').stream.submissions():
+        logging.debug('Starting submission {}'.format(sub_id))
         blockeduser = 0
         for item in reddit.inbox.messages(limit=5):
             if str(item.subject) == 'Remove me from posts':
@@ -13,6 +17,7 @@ def action():
                         newstring = username.replace(r'/u/','')
                         newstring = newstring = '\n'
                         file.write(newstring)
+        logger.debug('Cycle 1 for submission {} has finished'.format(sub_id))
         duplicates = []
         submission = praw.models.Submission(reddit, id = sub_id)
         with open('blockusers.txt','r') as newfile:
@@ -22,6 +27,7 @@ def action():
                     blockeduser = 1
                 else:
                     pass
+        logger.debug('Cycle 2 for submission {} has finished'.format(sub_id))
         if blockeduser == 0:
             for duplicate in submission.duplicates():            
                 dup_sub = praw.models.Submission(reddit, id = duplicate)
@@ -37,19 +43,40 @@ def action():
                         for dup in duplicates:
                             message = str(message + '\n * [{}]({}) on /r/{} (created at {} by {})').format(dup['title'], dup['link'], dup['subreddit'], dup['time'], dup['author'])
                         message = message + '\n\n ---- \n\n ^^I ^^am ^^a ^^bot  ^^[FAQ](https://www.reddit.com/r/DuplicatesBot/wiki/index)-[Code](https://github.com/PokestarFan/DuplicateBot-[Bugs](https://www.reddit.com/r/DuplicatesBot/comments/6ypgmx/bugs_and_problems/)-[Suggestions](https://www.reddit.com/r/DuplicatesBot/comments/6ypg85/suggestion_for_duplicatesbot/)-[Block](https://www.reddit.com/r/DuplicatesBot/wiki/index#wiki_block_bot_from_tagging_on_your_posts)'
+            logger.debug('Cycle 3 for submission {} has finished'.format(sub_id))
             try:
                 submission.reply(message)
+                logger.info('Message posted on {}'.format(sub_id))
+                logger.debug('Message content: \n {}'.format(message))
+                message = ''
+            except(praw.exceptions.APIException, UnboundLocalError):
+                logger.debug('Submission {} has been skipped due to being blocked or missing text'.format(sub_id), exc_info=True)
                 message = ''
             except:
-                pass
+                logger.error('Error occured!', exc_info=True)
                 message = ''
             
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARN)
+    logger = logging.getLogger(__name__)
+    handler = logging.FileHandler('debug.log')
+    handler.setLevel(logging.DEBUG)
+    handler2 = logging.FileHandler('info.log')
+    handler2.setLevel(logging.INFO)
+    handler3 = logging.FileHandler('errors.log')
+    handler3.setLevel(logging.WARN)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    handler2.setFormatter(formatter)
+    handler3.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.addHandler(handler2)
+    logger.addHandler(handler3)
     while True:
         try:
             action()
         except(KeyboardInterrupt):
             raise KeyboardInterrupt
         except:
-            pass
+            logger.critical('Error has occured when running main loop, please resolve asap', exc_info=True)
