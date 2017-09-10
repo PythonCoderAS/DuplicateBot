10dec00730f73d55390a8716cf7b11438ff1ba7c
PokestarFan <azoysarkar@gmail.com>
2017-09-10 08:51:26 -0400

    New file naming system

--- duplicate.py
+++ duplicate.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime as time
 import logging
 import praw
 import prawcore
@@ -6,12 +6,12 @@ import sys
 from login import reddit
 
 
-file_handler = logging.FileHandler(filename='duplicates.log')
+file_handler = logging.FileHandler(filename='duplicates_{}.log'.format(time.now().strftime('%m_%d_%y')))
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
-    level=logging.DEBUG, 
+    level=logging.INFO, 
     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
     handlers=handlers
 )
@@ -37,22 +37,27 @@ def action():
                 dup_sub = praw.models.Submission(reddit, id = duplicate)
                 if 'ImagesOf' not in str(dup_sub.subreddit) and 'auto' not in str(dup_sub.subreddit):
                     time = dup_sub.created
-                    time = str(datetime.datetime.fromtimestamp(time))
+                    time = str(time.fromtimestamp(time))
                     author = str(dup_sub.author)
                     if str(submission.author) == author:
                         author = author + '[author of both threads]'
-                    duplicates.append({'title':str(dup_sub.title), 'subreddit':str(dup_sub.subreddit), 'link':'https://www.reddit.com'+str(dup_sub.permalink), 'time':str(time), 'author':author})
+                    duplicates.append({'title':str(dup_sub.title), 'subreddit':str(dup_sub.subreddit), 'link':'https://www.reddit.com'+str(dup_sub.permalink), 'time':str(time), 'author':author, 'karma': str(dup_sub.score)})
                     if len(duplicates) > 0:
                         message = 'Here is a list of threads in other subreddits about the same content:\n'
                         for dup in duplicates:
-                            message = str(message + '\n * [{}]({}) on /r/{} (created at {} by {})').format(dup['title'], dup['link'], dup['subreddit'], dup['time'], dup['author'])
+                            message = str(message + '\n * [{}]({}) on /r/{} with {} karma (created at {} by {})').format(dup['title'], dup['link'], dup['subreddit'], dup['karma'],dup['time'], dup['author'])
                         message = message + '\n\n ---- \n\n ^^I ^^am ^^a ^^bot  ^^[FAQ](https://www.reddit.com/r/DuplicatesBot/wiki/index)-[Code](https://github.com/PokestarFan/DuplicateBot-[Bugs](https://www.reddit.com/r/DuplicatesBot/comments/6ypgmx/bugs_and_problems/)-[Suggestions](https://www.reddit.com/r/DuplicatesBot/comments/6ypg85/suggestion_for_duplicatesbot/)-[Block](https://www.reddit.com/r/DuplicatesBot/wiki/index#wiki_block_bot_from_tagging_on_your_posts)'
             try:
                 submission.reply(message)
                 logger.info('Message posted on {}'.format(sub_id))
+                logger.debug('Message: {}'.format(message))
                 message = ''
-            except(praw.exceptions.APIException, UnboundLocalError):
+            except(praw.exceptions.APIException, UnboundLocalError)as e:
                 logger.info('Submission {} has been skipped due to missing text'.format(sub_id))
+                try:
+                    logger.debug('Message: {}'.format(message))
+                except:
+                    logger.debug('Message output for exception ')
                 message = ''
             except(prawcore.exceptions.Forbidden):
                 logger.info('You are blocked on /r/{}'.format(str(submission.subreddit)))
