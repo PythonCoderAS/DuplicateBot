a06c15cd42b7ba07bee539fa38fc4bf92c4854d5
PokestarFan <azoysarkar@gmail.com>
2017-09-19 20:21:02 -0400

    Add logger level customization

--- modules/logger.py
+++ modules/logger.py
@@ -3,13 +3,13 @@ from datetime import datetime
 import sys
 
 
-def setup_logger(name):
+def setup_logger(name, loglevel=logging.INFO):
 	file_handler = logging.FileHandler(filename='logs/{}_{}.log'.format(name, datetime.now().strftime('%m_%d_%y')))
 	stdout_handler = logging.StreamHandler(sys.stdout)
 	handlers = [file_handler, stdout_handler]
 	
 	logging.basicConfig(
-		level=logging.INFO, 
+		level=loglevel, 
 		format='[%(asctime)s] {%(filename)s:%(lineno)d} (%(funcName)s) %(levelname)s - %(message)s',
 		handlers=handlers
 	)
